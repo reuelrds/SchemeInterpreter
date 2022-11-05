ea163dbd22527b0f75119581fb40b9f6875ba666
Reuel D'silva <reuelrds@gmail.com>
2022-11-05 07:46:23 -0500

    Added a method getInstance to Void class so that it is compatible with the reference binary files.

--- Tree/Void.py
+++ Tree/Void.py
@@ -15,3 +15,15 @@ class Void(Node):
 
     def print(self, n, p=False):
         pass
+
+    # This method is just for testing pruposes so that this class is compatible
+    # with the reference binary files
+    #
+    # TODO: Remove this method once the interpreter is implementated
+    @staticmethod
+    def getInstance():
+
+        if not Void._instance:
+            Void()
+
+        return Void._instance
